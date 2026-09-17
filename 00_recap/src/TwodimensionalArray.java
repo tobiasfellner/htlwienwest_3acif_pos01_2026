0de8bf87f2f160bdc809eef0a603ae98a663f5fc
tobiasfellner <tobias.fellner@me.com>
2026-09-17 16:54:29 +0200

    zweidimensionale arrays

---
+++ 00_recap/src/TwodimensionalArray.java
@@ -0,0 +1,8 @@
+public class TwodimensionalArray {
+    public static void main(String[] args) {
+        int[][]arr = new int[2][2];
+        int rows = arr.length;
+        int cols = arr[0].length;
+        arr[0][0] = 100;
+    }
+}
